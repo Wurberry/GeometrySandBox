31a5d732bac8fec24988719fa6393fc79b4fe73a
Daniil Evstigneev <dany.evstigneev@gmail.com>
2022-07-13 12:37:10 +0300

    Learned some things
    Learned how to work with FString
    OnScreenDebugMessage

--- Source/GeometrySandBox/Core/BaseGeometryActor.cpp
+++ Source/GeometrySandBox/Core/BaseGeometryActor.cpp
@@ -15,7 +15,8 @@ ABaseGeometryActor::ABaseGeometryActor()
 void ABaseGeometryActor::BeginPlay()
 {
 	Super::BeginPlay();
-	
+	PrintTypes();
+	PrintStringTypes();
 }
 
 // Called every frame
@@ -25,3 +26,32 @@ void ABaseGeometryActor::Tick(float DeltaTime)
 
 }
 
+void ABaseGeometryActor::PrintTypes()
+{
+	UE_LOG(LogTemp, Display, TEXT("Hello Unreal"));
+
+	int WeaponsNum = 4;
+	float Health = 34.32134f;
+	bool IsDead = false;
+
+	UE_LOG(LogTemp, Display, TEXT("Weapons Number - %d - %i, Health - %f, is Dead - %d"),
+		WeaponsNum, WeaponsNum, Health, IsDead);
+
+}
+
+void ABaseGeometryActor::PrintStringTypes()
+{
+	int WeaponsNum = 4;
+	float Health = 34.32134f;
+	bool IsDead = false;
+	
+	FString Name = "John Connor";
+	FString StrWeaponsNum = "Weapons num = " + FString::FromInt(WeaponsNum);
+	FString StrHealth = "Health count = " + FString::SanitizeFloat(Health);
+	FString StrIsDead = "Is Dead - " + FString(IsDead ? "trueee" : "falseeee");
+
+	FString Stat = FString::Printf(TEXT("All Stat \n %s \n %s \n %s"), *StrWeaponsNum, *StrHealth, *StrIsDead);
+	
+	GEngine->AddOnScreenDebugMessage(-1, 3.0f, FColor::Red, Name,true, FVector2D(1.5f,1.5f));
+
+}
--- Source/GeometrySandBox/Core/BaseGeometryActor.h
+++ Source/GeometrySandBox/Core/BaseGeometryActor.h
@@ -15,12 +15,16 @@ public:
 	// Sets default values for this actor's properties
 	ABaseGeometryActor();
 
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
+private:
 
+	static void PrintTypes();
+	static void PrintStringTypes();
+	
 };
